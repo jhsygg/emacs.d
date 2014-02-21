57497443c2321c1649fa80afa8f9d9657c5267e3
jhsygg <jhsygg@126.com>
2014-02-21 10:26:03 +0800

    add .Xmodmap

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-;; org-persist.el - jhsygg.hg232f at <2014-02-15 六 13:01>
-=======
-;; org-persist.el - jhsygg at <2014-02-17 一 09:12>
->>>>>>> dcfd10b90eb7399982a0bec7d9f85e6e241afd4b
+;; org-persist.el - localhost.localdomain at <2014-02-21 五 07:11>
